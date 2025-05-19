85cb7671ca70b72d36c107265a3b3d8c94fa7a50
sheikh-zafar <audios.shkzafar@gmail.com>
2025-05-19 14:16:00 +0400

    commit

--- app/duroos/quran-tafseer/page.tsx
+++
@@ -1,8 +0,0 @@
-import AudioPlayer from '@/components/AudioPlayer'
-import React from 'react'
-
-export default function QuranTafseer() {
-  return (
-    <div><AudioPlayer fileName="Tafseer Surah AL-LAIL-Part-1@date06-12-18.mp3"/></div>
-  )
-}
