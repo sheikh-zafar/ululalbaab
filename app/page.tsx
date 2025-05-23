68184a97c04ea81ff9aa8c67d8333f69f3fd3102
sheikh-zafar <audios.shkzafar@gmail.com>
2025-05-23 18:14:21 +0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -11,41 +11,7 @@ export default function Home() {
   return (
     <div>
       <HeaderSection />
-      <div className="max-w-7xl px-2 mx-auto my-20 xs:my-14 xxs:my-14 s:my-14">
-        <h6 className="text-dustygray text-center text-5xl lg:text-4xl sm:text-2xl xs:text-2xl xxs:text-2xl md:text-4xl font-extrabold font-sans">
-          About Sheikh zafarulhasan Madani
-        </h6>
-        <hr className="bg-bgcolor text-2xl" />
-        <div className="max-w-7xl mx-2 mt-5">
-        <div className="flex">
-          <div className="w-1/6">
-            <Image
-              src={logo}
-              alt="ahle ilm"
-              width="150"
-              height="80"
-            />
-          </div>
-          <div className="w-5/6">
-            <p className="text-text-primary text-lg md:text-base xs:text-sm xxs:text-sm my-4 xs:my-3 xxs:my-3">
-              Zafar-ul-Hasan Ahmedullah was born in 1956. He
-              hails from a religious family of eight members
-              from Changhaipur (Pratapgarh), a small town in
-              Uttar Pradesh, India. His father was a farmer
-              and Quran teacher, used to attend lectures of
-              Moulana Ahmedullah Pratapgadhi, Moulana
-              Sanaullah Amritseri...
-            </p>
-            <Link
-              href="/about"
-              className="bg-button-primary hover:bg-button-hover text-whiterock rounded-xl hover:shadow-2xl p-3 text-base font-bold font-mono "
-            >
-              Read more
-            </Link>
-          </div>
-        </div>
-      </div>
-      </div>
+      
       
       <BookCarousel />
       <SocialLinks/>
