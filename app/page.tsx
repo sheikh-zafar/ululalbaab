39e8926ce6ec9e69bb06c862ad8b860a2be43748
sheikh-zafar <audios.shkzafar@gmail.com>
2025-05-23 18:14:55 +0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
 import HeaderSection from "@/components/header"
-import Image from "next/image"
 import BookCarousel from "@/components/PopularSharh"
 import WeeklySchedule from "@/components/WeeklySchedule"
 import VisionMission from "@/components/VisionMission"
-import Link from "next/link"
-import logo from "../public/images/icon-192x192.png"
 import SocialLinks from "@/components/SocialMedia"
 
 export default function Home() {
   return (
     <div>
       <HeaderSection />
-      
-      
       <BookCarousel />
       <SocialLinks/>
       <WeeklySchedule />
